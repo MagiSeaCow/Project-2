49e833f7a34324d5ab908e226206dcd7d5dd61e0
irenebogues <irenebogues@gmail.com>
2019-02-11 14:34:07 -0500

    updating server.js

--- server.js
+++ server.js
@@ -1,30 +1,24 @@
-// require("dotenv").load();
-require("dotenv").config();
-
 //dependencies
 var express = require("express");
-// var exphbs = require("express-handlebars");
-// var bodyParser = require("body-parser");
-var cookieParser = require('cookie-parser');
+// Routes
+var app = express();
+var routes = require('./routes/api-routes');
+
+// require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
+
+
 
 // var passport = require("./config/passport");
 // var expressSession = require("express-session"); //sessions for cookies
+ var db = require("./models");
+// app.use(cookieParser())
 
-// var db = require("./models");
-var app = express();
-app.use(cookieParser())
 
-app.get('/', function (req, res) {
-  // Cookies that have not been signed
-  console.log('Cookies: ', req.cookies)
 
-  // Cookies that have been signed
-  console.log('Signed Cookies: ', req.signedCookies)
-})
+// app.listen(3000)
 
-app.listen(3000)
-
-// var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 
 // app.use(bodyParser.urlencoded({
@@ -63,15 +57,12 @@ app.listen(3000)
 //   syncOptions.force = true;
 // }
 
-// // Starting the server, syncing our models ------------------------------------/
-// db.sequelize.sync(syncOptions).then(function() {
-//   app.listen(PORT, function() {
-//     console.log(
-//       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-//       PORT,
-//       PORT
-//     );
-//   });
-// });
-
-// module.exports = app;
+ // Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync(syncOptions).then(function() {
+ app.listen(PORT, function() {
+  console.log(
+     "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser PORT" +
+    PORT);
+ });
+});
+module.exports = app;
