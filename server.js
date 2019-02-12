0a896cb849f1647a3b2942a760bba7e4cc387a71
irenebogues <irenebogues@gmail.com>
2019-02-12 11:51:07 -0500

    server testing

--- server.js
+++ server.js
@@ -1,43 +1,62 @@
-//dependencies
-var express = require("express");
-
-// Routes
+var express = require('express');
 var app = express();
-var routes = require('./routes/api-routes');
+ 
+ 
+app.get('/', function(req, res) {
+ 
+    res.send('Welcome to Passport with Sequelize');
+ 
+});
+ 
+ 
+app.listen(5000, function(err) {
+ 
+    if (!err)
+        console.log("Site is live");
+    else console.log(err)
+ 
+});
 
-var db = require("./models");
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// //dependencies
+// var express = require("express");
 
-var PORT = process.env.PORT || 8080;
+// // Routes
+// var app = express();
+// var routes = require('./routes/api-routes');
 
-app.use(passport.initialize());
-app.use(passport.session());
+// var db = require("./models");
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
+// var PORT = process.env.PORT || 8080;
 
-// Routes
-app.use(routes);
-require("./routes/apiRoutes")(app);
-app.use(routes);
-require("./routes/htmlRoutes")(app);
-
-
-//app use for public directory
-app.use(express.static("public"));
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 
+// // Routes
+// app.use(routes);
+// require("./routes/apiRoutes")(app);
+// app.use(routes);
+// require("./routes/htmlRoutes")(app);
 
- // Starting the server, syncing our models ------------------------------------/
- db.sequelize.sync().then(function() {
- app.listen(PORT, function() {
-console.log(
-  "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser PORT" +
-     PORT);
-    });
- });
+
+// //app use for public directory
+// app.use(express.static("public"));
+
+// // If running a test, set syncOptions.force to true
+// // clearing the `testdb`
+// if (process.env.NODE_ENV === "test") {
+//   syncOptions.force = true;
+// }
+
+
+
+//  // Starting the server, syncing our models ------------------------------------/
+//  db.sequelize.sync().then(function() {
+//  app.listen(PORT, function() {
+// console.log(
+//   "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser PORT" +
+//      PORT);
+//     });
+//  });
