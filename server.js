136318c03c6430550ff9ce75c632bc32e5e1f4a7
irenebogues <irenebogues@gmail.com>
2019-02-12 15:20:00 -0500

    initialzing app front page

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 5000;
- 
+//app use for public directory
+app.use(express.static("public")); 
 app.get('/', function(req, res) {
  
-    res.send('Welcome to Passport with Sequelize');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
  
 });
  
+app.get('/signup', function(req, res) {
  
+    res.sendFile(path.join(__dirname, 'public', 'signup.html'));
+ 
+});
 app.listen(PORT, function(err) {
  
     if (!err)
