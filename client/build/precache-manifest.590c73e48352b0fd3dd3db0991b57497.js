944e49ddb679c388654ed07b9f1b4adc8a4006e5
irenebogues <irenebogues@gmail.com>
2019-03-10 13:05:38 -0400

    restaging app

---
+++ client/build/precache-manifest.590c73e48352b0fd3dd3db0991b57497.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4a38c5327380540fa60",
+    "url": "/static/css/main.49d861fc.chunk.css"
+  },
+  {
+    "revision": "e4a38c5327380540fa60",
+    "url": "/static/js/main.bf49fb20.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "f33564666c79f3e88d0e77091dce2ede",
+    "url": "/index.html"
+  }
+];
